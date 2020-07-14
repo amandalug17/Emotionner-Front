e84b6c70ff13ee0ebbea0eb8b6723c1f278109c3
CharlesAndrea <charles.andrea@correo.unimet.edu.ve>
2020-07-14 18:39:17 -0400

    1

--- src/Components/Elements/tasksPerDay.js
+++ src/Components/Elements/tasksPerDay.js
@@ -1,5 +1,5 @@
 import React, { Component, useState} from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Label } from 'reactstrap'
 import AuthService from '../../Services/auth.service'
 import axios from 'axios'
 import './../../App.css'
@@ -7,11 +7,10 @@ import {Line} from 'react-chartjs-2';
 
 
 class TasksPerDay extends Component {
-    state = {
-        items: []
-    }
-
     
+    
+
+
     render() {
         return (
             <>
